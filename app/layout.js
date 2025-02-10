68d392d89c26e9b8d4e058b9f4c05a0dc2e4a1c6
shhady <shhadyse@gmail.com>
2025-02-10 14:47:09 +0200

    test

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,14 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono,Rubik } from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
+});
+const rubik = Rubik({
+  subsets: ['latin'],
+  weight: ['400', '500', '700'],
+  display: 'swap',
 });
 
 const geistMono = Geist_Mono({
@@ -20,7 +25,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${rubik.className} antialiased`}
       >
         {children}
       </body>
