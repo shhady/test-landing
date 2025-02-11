eab55ac5445fe1cfb0d17369fb7e12bfd35da763
shhady <shhadyse@gmail.com>
2025-02-11 14:19:48 +0200

    test

--- app/config/agents.js
+++ app/config/agents.js
@@ -6,7 +6,38 @@ export const allowedAgents = [
   },
   {
     id: 'shhadyserhan',
-    name: 'שחאדה סרחאן',
+    name: 'שחאדה סרחאן', 
     email: 'shhadyse@gmail.com'
-  }
+  },
+  {
+    id: 'tcpa',
+    name: 'רו״ח טנוס אסעד',
+    email: 'tcpa@gmail.com'
+  },
+  {
+    id: 'fadiadv',
+    name: 'פאדי שופאני',
+    email: 'fadiadv@gmail.com'
+  },
+  {
+    id: 'eliasadv',
+    name: 'אליאס קורדחגי',
+    email: 'eliasadv@gmail.com'
+  },
+  {
+    id: 'toptours',
+    name: 'טופ טורס',
+    email: 'toptours@gmail.com'
+  },
+  {
+    id: 'aimanj',
+    name: 'איימן גראיסי',
+    email: 'aimanj@gmail.com'
+  },
+  {
+    id: 'bsure',
+    name: 'באסל קורדחגי',
+    email: 'bsure@gmail.com'
+  },
+  
 ]; 
